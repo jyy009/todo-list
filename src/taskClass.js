d3137fa719b69704262c5e0811051b4133cd05c7
jyy009 <153378401+jyy009@users.noreply.github.com>
2024-10-31 20:38:15 +0100

    Create fx to add task to list from form

--- src/taskClass.js
+++ src/taskClass.js
@@ -1,9 +1,10 @@
 export class Task {
-  constructor(title, description, dueDate, isPriority, isComplete) {
+  constructor(title, description, dueDate, isPriority, isComplete, id) {
     this.title = title;
     this.description = description;
     this.dueDate = dueDate;
     this.isPriority = isPriority;
     this.isComplete = isComplete;
+    this.id = id;
   }
 }
--- src/taskLogic.js
+++ src/taskLogic.js
@@ -1,5 +1,6 @@
 import { format } from "date-fns";
 import { Task } from "./taskClass.js";
+import { id } from "date-fns/locale";
 
 export const taskLogic = () => {
 
@@ -20,39 +21,63 @@ let taskList = [
   }
 ]
 
-taskList.map(task => new Task(task.title, task.description, task.dueDate, task.isPriority))
+taskList.map(task => new Task(task.title, task.description, task.dueDate, task.isPriority, task.id))
 console.log("task list:", taskList)
 
-
+//display task view
 const taskViewTemplate = document.getElementById("task-view-template")
 const clone = taskViewTemplate.content.cloneNode(true)
 const taskSection = document.querySelector("#task-section")
 taskSection.appendChild(clone)
 
-
+//display task form
 const taskItemTemplate = document.getElementById("task-item-template")
 const taskItemClone = taskItemTemplate.content.cloneNode(true)
 taskSection.appendChild(taskItemClone)
 
-const addTaskToList = (title, description, dueDate, isPriority, isChecked) => {
-  const newTask = new Task(title, description, dueDate, isPriority, isChecked)
+const addTaskToList = (newTask) => {
+  // const newTask = new Task(title, description, dueDate, isPriority, isChecked, id)
   taskList.push(newTask)
   // console.log(newTask instanceof Task)
   console.log(taskList)
 }
 
-const taskForm = document.querySelector("#task-form")
-taskForm.addEventListener("submit", (e) => {
-  e.preventDefault()
+const resetTaskForm = () => {
+  
+}
 
-  const title = document.querySelector('input[name="task-title"]').value
-  const description = document.querySelector('input[description="task-description"]').value
-  const dueDate = document.querySelector('input[name="task-date"]').value
-  const isPriority = document.querySelector('input[name="task-priority"]').checked
-  const isComplete = document.querySelector('input[name="task-complete"]').checked
+
+const addTaskFromForm = (event) => {
+event.preventDefault()
+
+const form = event.target
+const newTask = new Task(
+  form.title.value,
+  form.description.value,
+  form.date.value,
+  form.priority.checked,
+  form.complete.checked,
+  crypto.randomUUID(),
+)
+
+addTaskToList(newTask)
+}
+
+const taskForm = document.querySelector("#task-form")
+taskForm.addEventListener("submit", addTaskFromForm)
+
+// const taskForm = document.querySelector("#task-form")
+// taskForm.addEventListener("submit", (e) => {
+//   e.preventDefault()
+
+//   const title = document.querySelector('input[name="task-title"]').value
+//   const description = document.querySelector('input[description="task-description"]').value
+//   const dueDate = document.querySelector('input[name="task-date"]').value
+//   const isPriority = document.querySelector('input[name="task-priority"]').checked
+//   const isComplete = document.querySelector('input[name="task-complete"]').checked
   
-  addTaskToList(title, description, dueDate, isPriority, isComplete)
+//   addTaskToList()
   
-})
+// })
 
 }
